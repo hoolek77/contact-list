a0dec7ac5bce4824362c0066b0a45dd02f9c06eb
hoolek77 <szymon.kin@gmail.com>
2022-12-30 18:04:49 +0100

    feat: Fetch data and display it in the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import apiData from './api'
 import PersonInfo from './PersonInfo'
 
-function App() {
-  const [data, setData] = React.useState([])
-  const [selected, setSelected] = React.useState([])
+type Person = {
+  id: string
+  jobTitle: string
+  emailAddress: string
+  firstNameLastName: string
+}
 
-  //  TODO fetch contacts using apiData function, handle loading and error states
+function App() {
+  const [data, setData] = useState<Person[]>([])
+  const [selected, setSelected] = useState([])
+
+  const fetchData = async () => {
+    try {
+      const data = await apiData()
+
+      setData((prev) => [...prev, ...data])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className="App">
       <div className="selected">Selected contacts: {selected.length}</div>
       <div className="list">
-        {/* {data.map((personInfo) => (
+        {data.map((personInfo) => (
           <PersonInfo key={personInfo.id} data={personInfo} />
-        ))} */}
+        ))}
+        <button onClick={fetchData}>Fetch More</button>
       </div>
     </div>
   )
