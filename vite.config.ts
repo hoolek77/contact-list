d22cfd59875a5bbfa00b7fcb2187f2162cadb2f7
hoolek77 <szymon.kin@gmail.com>
2023-01-01 23:49:48 +0100

    refactor: Migrate to Vite from CRA

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+})
